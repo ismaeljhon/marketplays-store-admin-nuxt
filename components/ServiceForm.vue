fff0296d32abe8dbce9d00f18861a665ced1152d
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-11-19 14:12:10 +0800

    update service form layout, add image handler UI

--- components/ServiceForm.vue
+++ components/ServiceForm.vue
@@ -1,142 +1,174 @@
 <template>
   <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
     <form @submit.prevent="handleSubmit(submit)">
-      <v-row>
-        <v-col cols="12">
-          <h3 class="mb-2">General Information</h3>
-          <v-divider></v-divider>
-        </v-col>
-        <v-col cols="6">
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="Name"
-            :rules="'required'"
-          >
-            <v-text-field v-model="form.name" :error-messages="errors">
-              <template slot="label">
-                Name <span class="red--text">*</span>
-              </template>
-            </v-text-field>
-          </ValidationProvider>
-          <v-text-field
-            v-model="form.slug"
-            label="Url"
-            hint="This would be used for pretty url"
-            persistent-hint
-            class="mb-3"
-          ></v-text-field>
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="Workforce Threshold"
-            :rules="'required|numeric'"
-          >
-            <v-text-field
-              v-model="form.workforceThreshold"
-              type="number"
-              :error-messages="errors"
-            >
-              <template slot="label">
-                Workforce Threshold <span class="red--text">*</span>
-              </template>
-            </v-text-field>
-          </ValidationProvider>
-          <v-text-field
-            v-model="form.shortDescription"
-            label="Short Description"
-          >
-          </v-text-field>
-        </v-col>
-        <v-col cols="6">
-          <v-autocomplete
-            v-model="form.department"
-            :items="departments"
-            hide-no-data
-            item-text="name"
-            item-value="_id"
-            label="Department"
-            placeholder="Select Department"
-          ></v-autocomplete>
-          <v-autocomplete
-            v-model="form.projectManager"
-            :items="users"
-            hide-no-data
-            item-text="fullName"
-            item-value="_id"
-            label="Product Manager"
-            placeholder="Select Product Manager"
-          ></v-autocomplete>
-          <ValidationProvider
-            v-slot="{ errors }"
-            name="Pricing"
-            :rules="'required|numeric'"
-          >
-            <v-text-field
-              v-model="form.pricing"
-              type="number"
-              :error-messages="errors"
-            >
-              <template slot="label">
-                Pricing <span class="red--text">*</span>
-              </template>
-            </v-text-field>
-          </ValidationProvider>
-          <v-autocomplete
-            v-model="form.tags"
-            :items="tags"
-            multiple
-            chips
-            deletable-chips
-            hide-selected
-            hide-no-data
-            label="Tags"
-          >
-            <template slot="append-outer">
-              <v-btn
-                fab
-                x-small
-                color="primary"
-                @click.prevent="$refs.addTagFormModal.show()"
+      <v-card>
+        <v-card-title>General Information</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-row>
+            <v-col cols="6">
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="Name"
+                :rules="'required'"
               >
-                <v-icon>mdi-plus</v-icon>
-              </v-btn>
-            </template>
-          </v-autocomplete>
-          <add-tag-form-modal ref="addTagFormModal" @save="updateTags" />
-        </v-col>
-        <v-col cols="12">
-          <p class="my-2">Description</p>
-          <vue-editor v-model="form.description"></vue-editor>
-        </v-col>
+                <v-text-field v-model="form.name" :error-messages="errors">
+                  <template slot="label">
+                    Name <span class="red--text">*</span>
+                  </template>
+                </v-text-field>
+              </ValidationProvider>
+              <v-text-field
+                v-model="form.slug"
+                label="Url"
+                hint="This would be used for pretty url"
+                persistent-hint
+                class="mb-3"
+              ></v-text-field>
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="Workforce Threshold"
+                :rules="'required|numeric'"
+              >
+                <v-text-field
+                  v-model="form.workforceThreshold"
+                  type="number"
+                  :error-messages="errors"
+                >
+                  <template slot="label">
+                    Workforce Threshold <span class="red--text">*</span>
+                  </template>
+                </v-text-field>
+              </ValidationProvider>
+              <v-text-field
+                v-model="form.shortDescription"
+                label="Short Description"
+              >
+              </v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-autocomplete
+                v-model="form.department"
+                :items="departments"
+                hide-no-data
+                item-text="name"
+                item-value="_id"
+                label="Department"
+                placeholder="Select Department"
+              ></v-autocomplete>
+              <v-autocomplete
+                v-model="form.projectManager"
+                :items="users"
+                hide-no-data
+                item-text="fullName"
+                item-value="_id"
+                label="Product Manager"
+                placeholder="Select Product Manager"
+              ></v-autocomplete>
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="Pricing"
+                :rules="'required|numeric'"
+              >
+                <v-text-field
+                  v-model="form.pricing"
+                  type="number"
+                  :error-messages="errors"
+                >
+                  <template slot="label">
+                    Pricing <span class="red--text">*</span>
+                  </template>
+                </v-text-field>
+              </ValidationProvider>
+              <v-autocomplete
+                v-model="form.tags"
+                :items="tags"
+                multiple
+                chips
+                deletable-chips
+                hide-selected
+                hide-no-data
+                label="Tags"
+              >
+                <template slot="append-outer">
+                  <v-btn
+                    fab
+                    x-small
+                    color="primary"
+                    @click.prevent="$refs.addTagFormModal.show()"
+                  >
+                    <v-icon>mdi-plus</v-icon>
+                  </v-btn>
+                </template>
+              </v-autocomplete>
+              <add-tag-form-modal ref="addTagFormModal" @save="updateTags" />
+            </v-col>
+            <v-col cols="12">
+              <p class="my-2">Description</p>
+              <vue-editor v-model="form.description"></vue-editor>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
 
-        <v-col cols="12" class="mt-5">
-          <h3 class="mb-2">SEO</h3>
-          <v-divider></v-divider>
-        </v-col>
-        <v-col cols="6">
-          <v-text-field
-            v-model="form.seoTitle"
-            label="SEO Title"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="6">
-          <v-text-field
-            v-model="form.seoKeywords"
-            label="SEO Keywords"
-            hint="Enter keywords related to your form."
-            persistent-hint
-            class="mb-3"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12">
-          <v-textarea
-            v-model="form.seoDescription"
-            hint="Type a description that summarizes your form.."
-            persistent-hint
-            class="mb-5"
-            label="SEO Description"
-          ></v-textarea>
-        </v-col>
-      </v-row>
+      <v-card class="mt-5">
+        <v-card-title>Images</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" class="text-center">
+              <handy-uploader
+                card-type="tile"
+                :document-attachment.sync="productImageAttachments"
+                file-uploader-type="simple"
+                :max-file-size="10240"
+                :image-compressor="true"
+                :image-compress-level="0.8"
+                :max-file-count="10"
+                badge-counter
+                change-file-name
+                add-file-description
+                file-accept="image/png,image/gif,image/jpeg,image/webp"
+                :custom-lang="productImageAttachmentsCustomLang"
+                lang="custom"
+              ></handy-uploader>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+
+      <v-card class="mt-5">
+        <v-card-title>SEO</v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="form.seoTitle"
+                label="SEO Title"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="form.seoKeywords"
+                label="SEO Keywords"
+                hint="Enter keywords related to your form."
+                persistent-hint
+                class="mb-3"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                v-model="form.seoDescription"
+                hint="Type a description that summarizes your form.."
+                persistent-hint
+                class="mb-5"
+                label="SEO Description"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
 
       <div class="mt-2">
         <v-btn @click.prevent="back">cancel</v-btn>
@@ -149,12 +181,14 @@
 import gql from 'graphql-tag'
 import _assign from 'lodash/assign'
 import { VueEditor } from 'vue2-editor'
+import handyUploader from 'handy-uploader/src/components/handyUploader'
 
 export default {
   // eslint-disable-next-line vue/name-property-casing
   name: 'service-form',
   components: {
     VueEditor,
+    handyUploader,
   },
   apollo: {
     users: {
@@ -210,6 +244,29 @@ export default {
       department: null,
     },
     tags: ['seo', 'web', 'web development', 'web design', 'graphics'],
+    productImageAttachments: [],
+    productImageAttachmentsCustomLang: {
+      custom: {
+        insertFile: 'Insert Image(s)',
+        insertNewFile: 'Click here to add image(s)',
+        add: 'Add Image(s)',
+        delete: 'Delete',
+        edit: 'Update Image',
+        deleteDialog: {
+          message: 'Are you sure you want to delete the image?',
+          cancel: 'cancel',
+        },
+        size: {
+          kb: 'KB',
+          mb: 'MB',
+        },
+        maxFileSizeAlert: 'Max file Size is',
+        maxFileCountAlert: 'Max file Count is',
+        fileName: 'File Name',
+        fileDescription: 'File Description',
+        fileTags: 'File Tags',
+      },
+    },
   }),
   watch: {
     service(value) {
@@ -257,7 +314,10 @@ export default {
         'projectManager',
         'department',
       ])
+      // eslint-disable-next-line no-console
+      return console.log(allowedItems)
 
+      // eslint-disable-next-line no-unreachable
       let result = null
       if (this.service) {
         result = await this.updateMutation(
