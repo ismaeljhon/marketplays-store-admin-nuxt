71fdf605fa5748a85d6ff1a6c9b9ad0299680839
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2020-11-19 21:35:30 +0800

    Add no data component

---
+++ components/NoDataMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-alert prominent text type="warning">
+    {{ message }}
+  </v-alert>
+</template>
+<script>
+export default {
+  name: 'NoDataMessage',
+  props: {
+    message: {
+      type: String,
+      default: 'No Data Available',
+    },
+  },
+}
+</script>
