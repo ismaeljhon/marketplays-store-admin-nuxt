0ab4777018c94e185a8817c53bc040b06ecd9f47
Ismael Jalbuna <jalbuna.ismael@gmail.com>
2021-01-03 21:09:17 +0800

    update config placeholders

--- config.sample.js
+++ config.sample.js
@@ -1,3 +1,14 @@
 export default {
-  apiUrl: 'http://localhost:5001',
+  production: {
+    BASE_URL: 'BASE_URL_HERE',
+    API_BASE_URL: 'graphql_URL_HERE',
+  },
+  staging: {
+    BASE_URL: 'BASE_URL_HERE',
+    API_BASE_URL: 'graphql_URL_HERE',
+  },
+  dev: {
+    BASE_URL: 'BASE_URL_HERE',
+    API_BASE_URL: 'graphql_URL_HERE',
+  },
 }
